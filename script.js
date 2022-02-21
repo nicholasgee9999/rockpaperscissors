a4e4b7f4280fc540bf863df7c3b5589d0f65e025
Nicholas Gee <nicholasgee9999@gmail.com>
2022-02-21 23:13:52 +0000

    fully working one round game

--- script.js
+++ script.js
@@ -14,16 +14,34 @@ if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"
 let computerPlay = choices [Math.floor(Math.random() *3)];
 console.log("Computer chose " + computerPlay);
 
-//declare winner
+//play round
 
-const winner = declareWinner (userChoice, computerPlay);
+const winner = playRound (userChoice, computerPlay);
  
-function declareWinner (userChoice, computerPlay) {
-    if (userChoice === "rock" && computerPlay === "paper") {
-        console.log("You Lose, paper beats rock!");
-    } else if (userChoice === "rock" && computerPlay === "scissors") {
-        console.log("You Win, rock beats scissors!");
-    } else {
-        console.log("You Tie");
-     }
-}
+function playRound (userChoice, computerPlay) {
+    if (userChoice === "rock") {
+        if (computerPlay === "rock"){
+            console.log("it's a tie!");
+        } else if (computerPlay === "paper") {
+            console.log("you lose, paper beats rock!")
+        } else if (computerPlay === "scissors") {
+            console.log("you win, rock beats scissors!");
+        }
+    } else if (userChoice === "paper") {
+        if (computerPlay === "rock"){
+            console.log("you win, paper beats rock!");
+        } else if (computerPlay === "scissors") {
+            console.log("you lose, scissors beat paper!")
+        } else if (computerPlay === "paper") {
+            console.log("it's a tie!")
+        }
+    } else if (userChoice === "scissors") {
+        if (computerPlay === "scissors") {
+            console.log("it's a tie!");
+        } else if (computerPlay === "rock") {
+            console.log("you win, scissors beat rock!")
+        } else if (computerPlay === "paper") {
+            console.log("you lose, paper beats scissors!")
+        }
+        }
+    }
