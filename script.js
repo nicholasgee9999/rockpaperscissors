b435df94d12c8e861388ca24565565c19d560b31
Nicholas Gee <nicholasgee9999@gmail.com>
2022-02-21 20:26:50 +0000

    player selection and computer play

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let userChoice = console.log("Rock, Paper, Scissors?")
+const choices = ["Rock", "Paper", "Scissors"];
+
+//player selection //
+
+const userChoice = prompt ("Rock, Paper, Scissors");
+if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"){
+    console.log("You chose " + userChoice); 
+    } else {
+        console.log("Error! Try again!");
+}
 
 //computer play//
 
-let computerPlay = Math.random();
-if (computerPlay <= 0.34){
-        computerPlay = "Rock";
-} else if
-    (computerPlay <= 0.67){
-        computerPlay = "Paper";
-} else {
-        computerPlay = "Scissors";
-}
+let computerPlay = choices [Math.floor(Math.random() *3)];
+console.log("Computer chose " + computerPlay);
 
+//game//
