43af30a421a34b1b49b740a9fdc1201af9fe0c14
Nicholas Gee <nicholasgee9999@gmail.com>
2022-02-21 22:35:33 +0000

    working code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
-//player selection //
+//player selection
 
 const userChoice = prompt ("Rock, Paper, Scissors");
 if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"){
@@ -9,9 +9,21 @@ if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"
         console.log("Error! Try again!");
 }
 
-//computer play//
+//computer play
 
 let computerPlay = choices [Math.floor(Math.random() *3)];
 console.log("Computer chose " + computerPlay);
 
-//game//
+//declare winner
+
+const winner = declareWinner (userChoice, computerPlay);
+ 
+function declareWinner (userChoice, computerPlay) {
+    if (userChoice === "rock" && computerPlay === "paper") {
+        console.log("You Lose, paper beats rock!");
+    } else if (userChoice === "rock" && computerPlay === "scissors") {
+        console.log("You Win, rock beats scissors!");
+    } else {
+        console.log("You Tie");
+     }
+}
