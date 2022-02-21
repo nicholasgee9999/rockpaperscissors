d7fcd9c2a70854ddbcc035af8d3dc6812e369785
Nicholas Gee <nicholasgee9999@gmail.com>
2022-02-21 19:30:48 +0000

    created computerPlay

---
+++ script.js
@@ -0,0 +1,14 @@
+let userChoice = console.log("Rock, Paper, Scissors?")
+
+//computer play//
+
+let computerPlay = Math.random();
+if (computerPlay <= 0.34){
+        computerPlay = "Rock";
+} else if
+    (computerPlay <= 0.67){
+        computerPlay = "Paper";
+} else {
+        computerPlay = "Scissors";
+}
+
